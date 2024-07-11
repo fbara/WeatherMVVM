6769c20cb03d2548eab69fc2051be5a8e503277f
fbara <fbara1@yahoo.com>
2024-07-11 12:42:31 -0400

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Frank Bara on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
