5e2223c664c402069bc34043cf2befafa7894283
fbara <fbara1@yahoo.com>
2024-07-12 10:39:09 -0400

    Created Weather model.

---
+++ Weather/ViewModels/WeatherViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherViewModel.swift
+//  Weather
+//
+//  Created by Frank Bara on 7/12/24.
+//
+
+import Foundation
---
+++ Weather/Weather.swift
@@ -0,0 +1,8 @@
+//
+//  Weather.swift
+//  Weather
+//
+//  Created by Frank Bara on 7/12/24.
+//
+
+import Foundation
