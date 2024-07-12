b4efb1b49e9aa59367d1793092990fb992613ca9
fbara <fbara1@yahoo.com>
2024-07-12 15:17:58 -0400

    Final commit.

---
+++ WeatherTests/WeatherTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherTests.swift
+//  WeatherTests
+//
+//  Created by Frank Bara on 7/12/24.
+//
+
+import Testing
+
+struct WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
